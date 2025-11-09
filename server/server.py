f8a1402ad436ebc573ee9018939326f4b4d24a37
masalgaro <masalgaro@eafit.edu.co>
2025-11-09 15:04:06 -0500

    Updated gitignore to ease development and tidy up the repository

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 # Main server program: it will send pings to all stored IPs
 # Needs a way to store IPs in a file it can read over and over
 
+import icmplib as icmp
 from StoreIP import StoreIPAddress as SIP 
 
 SIP.defineStorageFile("Direcciones.txt")
@@ -9,7 +10,15 @@ if (SIP.writeIPAddress("127.0.0.1", "Direcciones.txt") == 1):
 else:
     print("Vida hp")
 
-# lets do the code for reals lmao
+# let's do the code for reals lmao
 
-def pingKnownIPs():
+def updateIPList(IPAddressFile: str) -> list:
+    with open(IPAddressFile, 'r') as addressList:
+        IPs = file.read().splitlines()
     pass
+
+def pingKnownIPs(IPlist: str) -> None:
+    pass
+
+# the actual listening to IPs thing should happen always so idk if a separate function would really be needed.
+

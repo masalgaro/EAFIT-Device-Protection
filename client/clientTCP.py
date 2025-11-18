6912f8d8d66275b56895cf3f2ac57bb3da74b8d0
masalgaro <masalgaro@eafit.edu.co>
2025-11-17 20:03:14 -0500

    Changes in threading functionality

--- client/clientTCP.py
+++ client/clientTCP.py
@@ -3,21 +3,23 @@
 import socket, time
 
 print("[INFO]Este es el programa para los clientes/dispositivos de uso público.\n")
-host = socket.gethostname()
-hostSend = bytes(host, "utf-8")
-port = 21115
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect((host, port))
+
 while True:
+    host = socket.gethostname()
+    hostSend = bytes(host, "utf-8")
+    port = 21115
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect((host, port))
     try:
         cliente.sendall(hostSend) # Send the host name directly.
         print("[DEBUG]Enviado el nombre del host al servidor.\n")
         data = cliente.recv(1024)
         print("[DEBUG]Recibido ", data, " del servidor.\n")
-        time.sleep(3)
+        time.sleep(10)
     
     except socket.error:
         print("[ERROR]Error de socket.\n")
         break
+
 cliente.close()
-print("[INFO]Terminando programa del cliente.\nEjecute el script nuevamente para que el servidor pueda rastrear el dispositivo de forma correcta.")
+print("[ALERTA]Terminando programa del cliente.\nEjecute el script nuevamente para que el servidor pueda rastrear el dispositivo de forma correcta.")
--- server/serverTCP.py
+++ server/serverTCP.py
@@ -23,33 +23,33 @@ def updateHostList(HostNameFile: str) -> list[str] or None:
 
 
 def socketPing(interface: str, port: int) -> None:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((interface, port))
-    print(f"{interface}, {port}")
-
     s.listen(2)
+    print("[INFO]Esperando conexiones...")
     conn, address = s.accept()
     print(f"Conectado con {address}\n")
-    while True:
-        try:
-            data = conn.recv(1024) # Receive the host name of the client
-            if not data:
-                print(f"[ALERTA]Socket {conn} de la dirección {address} NO responde y puede estar en riesgo.\n")
-            else:
-                print("[DEBUG]Mensaje recibido: ", data, "\n") # Data should be the host name of the client we're connected to
-                conn.sendall("Echo")
-                clientesActivos.append(data) # Add the host to the known hosts list
-                SIP.writeIPAddress(data, archivoHosts) # Add the host to the storage file
+    try:
+        data = conn.recv(1024) # Receive the host name of the client
+        if not data:
+            print(f"[ALERTA]Socket {conn} de la dirección {address} NO responde y puede estar en riesgo.\n")
+        else:
+            print("[DEBUG]Mensaje recibido: ", data, "\n") # Data should be the host name of the client we're connected to
+            #conn.sendall("Echo")
+            clientesActivos.append(data) # Add the host to the known hosts list
+            #SIP.writeIPAddress(data, archivoHosts) # Add the host to the storage file
+            #continue # force continue on this bitch frfr
+            pass # Skip ahead to the next call of this function
 
-        except socket.error:
-            print("[ERROR]Error de socket.\n")
-            break
+    except socket.error:
+        print("[ERROR]Error de socket.\n")
+        #break
 
     conn.close()
+    print("[DEBUG]Conexión Terminada")
+    pass
 
 
 if __name__ == "__main__":
-    print("[INFO]Esta versión del programa funciona con sockets TCP, sin embargo requiere que cada host tenga un proceso de cliente activo en todo momento.\n")
+    print("[INFO]Esta versión del programa funciona con sockets TCP, y requiere que cada host tenga un proceso de cliente activo en todo momento.\n")
     clientesActivos = []
     archivoHosts = ''
 
@@ -62,7 +62,23 @@ if __name__ == "__main__":
     host = '' # We will listen from all interfaces
     port = 21115 # Just a random socket that isn't well-known or widely used.
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_THREAD_AMMOUNT) as exe:
-        exe.submit(socketPing, host, port) # Creates threads as needed to ping
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    print(f"[INFO]Servidor conectado en la interfaz {host}, y el puerto: {port}")
+
+    for i in range(MAX_THREAD_AMMOUNT):
+        i = threading.Thread(target=socketPing, args=(host, port))
+        i.start()
+        clientesActivos.append(i)
+
+    for j in clientesActivos:
+        j.join()
+
+    
+    """with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_THREAD_AMMOUNT) as exe:
+        while True:
+             exe.submit(socketPing, host, port) # Creates threads as needed to ping
+             #exe.shutdown() # idfk"""
+
 
     #socketPing(interface=host, port=port)
